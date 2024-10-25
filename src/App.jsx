790366da0e71b6ced4ab73b9873df2aaf2ea6781
Fitano Pranata <fitanopranata@gmail.com>
2024-10-25 10:55:43 +0700

    coming soon

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Login from './views/Login'
+import comingSoon from './assets/coming-soon.png'
 
 function App() {
 
   return (
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Login />} />
-          </Routes>
-      </BrowserRouter>
+    <div className="flex justify-center bg-[#ffd000] h-screen">
+      <img src={comingSoon} className="h-[80%] md:h-full" />
+    </div>
   )
 }
 
